0a646244b68f93c4a867861506ce9ccd9ed5fe2f
Candice Bianca Brown <candicebrown97@gmail.com>
2018-03-14 06:23:46 +0000

    initial commit -- completed video 4, working with Node

---
+++ app.js
@@ -0,0 +1,13 @@
+require('./instanthello'); //best practice to not use .js
+var goodbye = require('./talk/goodbye'); //in order to get access to call the function, we need to require to goodbye.js file and assign it thru a variable - via the path to the goodbye file
+
+var talk = require('./talk'); //for require, if you have an index.js file inside of a folder, no need to specify the file name itself just the parent folder name
+var question = require('./talk/question');
+
+talk.intro();
+talk.hello("Candice");
+
+var answer = question.ask("What is the meaning of life?");
+console.log(answer);
+
+goodbye();
---
+++ talk/goodbye.js
@@ -0,0 +1,4 @@
+//to return a function, must use module.exports
+module.exports = function(){
+    console.log("Goodbye");
+};
