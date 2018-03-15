aa3798ab6c86326acbd5f237050237aecd9fc174
Candice Bianca Brown <candicebrown97@gmail.com>
2018-03-15 20:15:32 +0000

    completed Lecture9 - serving static files

--- app.js
+++ app.js
@@ -21,18 +21,28 @@ var path = require('path');
 
 app.set('port', process.env.PORT);
 
-app.get('/', function(req, res){
-    console.log("GET the homepage");
-    res.status(200);
-    // res.send("Express Yourself");
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// in public folder: this is where we will store all of our static resources
+//after the port definition but before the root is defined, we define the static folder using the method app.use which introduces middleware
+app.use(express.static(path.join(__dirname, 'public'))); //when express receives a request for a root: 1)it will check to see if that root is matched by any of the files within that public folder; if it finds a match, it will deliver that file directly to the browser without any need to add in any extra routes
+
+// app.use('/public', express.static(path.join(__dirname, 'public')));
+//within app.use, to define a subset of routes - example: public/index.html
+//we need to specify the folder structure we want before we define the express static method
+//when launched, express will look for static routes that start with /public
+
+// app.get('/', function(req, res){ //due to adding the app use method, we had to get rid of this current homepage route to ensure we are getting the index.html file from the above static approach
+//     console.log("GET the homepage");
+//     res.status(200);
+//     // res.send("Express Yourself");
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 app.get('/json', function(req, res){
     console.log("GET the json");
     res.status(200);
     res.json( {"jsonData" : true});
 });
+
 
 //configure Express to return a file (delivered as is) to the Browser - instead of .send, use .sendFile method and pass it the path of the file you want to send
 app.get('/file', function(req, res){
