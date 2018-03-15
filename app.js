d69e8e42e4784014d7a552290d14f40cba48d540
Candice Bianca Brown <candicebrown97@gmail.com>
2018-03-15 02:31:33 +0000

    working with package.json and managing dependencies

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-require('./instanthello'); //best practice to not use .js
-var goodbye = require('./talk/goodbye'); //in order to get access to call the function, we need to require to goodbye.js file and assign it thru a variable - via the path to the goodbye file
+// require('./instanthello'); //best practice to not use .js
+// var goodbye = require('./talk/goodbye'); //in order to get access to call the function, we need to require to goodbye.js file and assign it thru a variable - via the path to the goodbye file
 
-var talk = require('./talk'); //for require, if you have an index.js file inside of a folder, no need to specify the file name itself just the parent folder name
-var question = require('./talk/question');
+// var talk = require('./talk'); //for require, if you have an index.js file inside of a folder, no need to specify the file name itself just the parent folder name
+// var question = require('./talk/question');
 
-talk.intro();
-talk.hello("Candice");
+// talk.intro();
+// talk.hello("Candice");
 
-var answer = question.ask("What is the meaning of life?");
-console.log(answer);
+// var answer = question.ask("What is the meaning of life?");
+// console.log(answer);
 
-goodbye();
+// goodbye();
+
+console.log("It works!"); //based on package.json dependencies scripts section -- from command line, entered npm start
